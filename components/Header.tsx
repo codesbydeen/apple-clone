ae6087ce6fd4e2119dc5d6c0725eb7200112a063
Geraldine <dinegerald10@gmail.com>
2022-09-15 10:15:14 +0200

    Create Apple Clone

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <Image src="https://rb.gy/vsvv2o" layout='fill' objectFit='contain'/>
+      </div>
+    </header>
+  )
+}
+
+export default Header
