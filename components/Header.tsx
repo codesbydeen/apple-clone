36077b667d11dab485f236349c3a6b6ca0a65e8a
Geraldine <dinegerald10@gmail.com>
2022-09-17 11:17:44 +0200

    Building Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+
+import {
+  SearchIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
+  const session = false;
+
   return (
-    <header>
-      <div>
-        <Image src="https://rb.gy/vsvv2o" layout='fill' objectFit='contain'/>
+    <header className='sticky top-0 z-30 flex w-full items-center justify-between
+    bg-[#E7ECEE] p-4 '>
+      <div className='flex items-center justify-center md:w-1/5'>
+        <Link href='/'>
+          <div className='relative h-10 w-5 cursor-pointer opacity-75 transition
+          hover:opacity-100'>
+            <Image src="https://rb.gy/vsvv2o" layout='fill' objectFit='contain'/>
+          </div>
+        </Link>
+      </div>
+      
+      <div className='hidden flex-1 items-center justify-center space-x-8 md:flex'>
+        <a className='headerLink'>Product</a>
+        <a className='headerLink'>Explore</a>
+        <a className='headerLink'>Support</a>
+        <a className='headerLink'>Business</a>
+      </div>
+
+      <div className='flex items-center justify-center gap-x-4 md:w-1/5'>
+        <SearchIcon className='headerIcon'/>
+        <Link href="/checkout">
+
+        <div className='relative cursor-pointer'>
+          <span className='absoulte -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center
+          rounded-full bg-gradient-to-r from-pink-500 to-violet-500'>5</span>
+          <ShoppingBagIcon className='headerIcon' />
+        </div>
+
+        </Link>
       </div>
     </header>
   )
