955d7f83dae0b5d00427b5e149e41301a8548e4b
Geraldine <dinegerald10@gmail.com>
2022-09-20 09:42:47 +0200

    Building Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,7 @@ function Header() {
 
         <div className='relative cursor-pointer'>
           <span className='absoulte -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center
-          rounded-full bg-gradient-to-r from-pink-500 to-violet-500'>5</span>
+          rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white'>5</span>
           <ShoppingBagIcon className='headerIcon' />
         </div>
 
