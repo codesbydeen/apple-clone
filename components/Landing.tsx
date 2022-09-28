9f2fbc648b3cffcacf20cafda68602e3292f29e0
Geraldine <dinegerald10@gmail.com>
2022-09-28 11:21:25 +0200

    Create Landing Page

---
+++ components/Landing.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+        <section className='sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center
+        justify-between px-8'>
+            <div className='space-y-8'>
+                <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
+                <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
+            Powered
+          </span>
+                    <span className='block'>By Intellect</span>
+                    <span className='block'>Driven By Values</span>
+                </h1>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Landing
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Landing from '../components/Landing'
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,10 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+
+      <main>
+        <Landing />
+      </main>
     </div>
   )
 }
